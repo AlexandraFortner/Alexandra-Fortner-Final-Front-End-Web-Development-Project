18c5f0da4254347161e5d99b8d1c8ad09395e29e
Alexandra <afortner@basecampcodingacademy.org>
2017-11-15 14:55:49 -0600

    Initial Commit.
    I started making inventory items., making them a list of dictionaries.
    
    I plan on making a for loop to display them all on screen next.

---
+++ index.js
@@ -0,0 +1,26 @@
+// MAIN DATA STARTS
+var INVENTORY = [
+    { name: 'Batman: The Killing Joke', price: 16.28 },
+    { name: 'Batman: Noël', price: 18.39 },
+    { name: 'Batman: Year One', price: 5.93 },
+    { name: 'Wolverine Goes To Hell', price: 10.23 },
+    { name: 'The Death Of Superman', price: 12.19 },
+    { name: 'Batman: Hush', price: 7.21 },
+    { name: 'Wonder Woman: The Hiketeia', price: 17.31 },
+    { name: 'Planet Hulk', price: 23.3 },
+    { name: 'Avengers: Time Runs Out', price: 21.22 },
+    { name: 'Deadpool Kills The Marvel Universe', price: 18.43 }
+];
+
+// MAIN DATA ENDS
+// FOR LOOPS START
+
+// FOR LOOPS END
+
+function main() {
+    $('#inventory').html(
+        'Game Over <i class="fa fa-gamepad" aria-hidden="true"></i>'
+    );
+}
+
+$(main);
