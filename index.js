6544489006df3271504200e2824ec61692de16ed
Alexandra <afortner@basecampcodingacademy.org>
2017-11-15 15:30:48 -0600

    Made shopping cart a button, added "Add to Cart" and "Remove From Cart" buttons.
    I might plan to make a sleek looking inventory panel at the bottom or side.
    
    I am now displaying the items, I might use my box class and just make the borders and background transparent.

--- index.js
+++ index.js
@@ -7,20 +7,33 @@ var INVENTORY = [
     { name: 'The Death Of Superman', price: 12.19 },
     { name: 'Batman: Hush', price: 7.21 },
     { name: 'Wonder Woman: The Hiketeia', price: 17.31 },
-    { name: 'Planet Hulk', price: 23.3 },
+    { name: 'Planet Hulk', price: 23.32 },
     { name: 'Avengers: Time Runs Out', price: 21.22 },
-    { name: 'Deadpool Kills The Marvel Universe', price: 18.43 }
+    { name: 'Deadpool Kills The Marvel Universe', price: 18.43 },
+    { name: 'Sgt. Fury and his Howling Commandos: Volume 1', price: 26.75 }
 ];
 
 // MAIN DATA ENDS
 // FOR LOOPS START
 
+function show_inventory() {
+    inventory = '';
+    for (i = 0; i < INVENTORY.length; i++) {
+        inventory +=
+            '<p><b>&nbsp;&nbsp;' +
+            INVENTORY[i].name +
+            '<br>$' +
+            INVENTORY[i].price +
+            '<br><button id="AddToCart">Add To Cart</button><br><button id="RemoveFromCart">Remove From Cart</button>';
+        ('</p>');
+    }
+    $('#Inventory-List').html(inventory);
+}
+
 // FOR LOOPS END
 
 function main() {
-    $('#inventory').html(
-        'Game Over <i class="fa fa-gamepad" aria-hidden="true"></i>'
-    );
+    show_inventory();
 }
 
 $(main);
