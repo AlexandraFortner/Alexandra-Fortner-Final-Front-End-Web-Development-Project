cafb35d3ef75d9765a9e56f23db5fc837877fcd1
Alexandra <afortner@basecampcodingacademy.org>
2017-11-16 09:34:35 -0600

    Added new alert messages(NOT MINE), and have AddToCart function working.
    I need to finish Remove From Cart function and set limits for it.

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
 // MAIN DATA STARTS
 var INVENTORY = [
-    { name: 'Batman: The Killing Joke', price: 16.28 },
-    { name: 'Batman: Noël', price: 18.39 },
-    { name: 'Batman: Year One', price: 5.93 },
-    { name: 'Wolverine Goes To Hell', price: 10.23 },
-    { name: 'The Death Of Superman', price: 12.19 },
-    { name: 'Batman: Hush', price: 7.21 },
-    { name: 'Wonder Woman: The Hiketeia', price: 17.31 },
-    { name: 'Planet Hulk', price: 23.32 },
-    { name: 'Avengers: Time Runs Out', price: 21.22 },
-    { name: 'Deadpool Kills The Marvel Universe', price: 18.43 },
-    { name: 'Sgt. Fury and his Howling Commandos: Volume 1', price: 26.75 }
+    { name: 'Batman: The Killing Joke', price: 16.28, inStock: 20 },
+    { name: 'Batman: Noël', price: 18.39, inStock: 24 },
+    { name: 'Batman: Year One', price: 5.93, inStock: 22 },
+    { name: 'Wolverine Goes To Hell', price: 10.23, inStock: 12 },
+    { name: 'The Death Of Superman', price: 12.19, inStock: 9 },
+    { name: 'Batman: Hush', price: 7.21, inStock: 16 },
+    { name: 'Wonder Woman: The Hiketeia', price: 17.31, inStock: 34 },
+    { name: 'Planet Hulk', price: 23.32, inStock: 6 },
+    { name: 'Avengers: Time Runs Out', price: 21.22, inStock: 10 },
+    { name: 'Deadpool Kills The Marvel Universe', price: 18.43, inStock: 17 },
+    {
+        name: 'Sgt. Fury and his Howling Commandos: Volume 1',
+        price: 26.75,
+        inStock: 4
+    }
 ];
+
+var CART = [];
 
 // MAIN DATA ENDS
 // SMALL FUNCTIONS START
 function AddToCart(cart_item) {
-    var counter = 0;
-    counter += 1;
-    console.log(counter);
-    console.log(INVENTORY[cart_item].name + ' was added to the cart!');
+    if (INVENTORY[cart_item].inStock === 0) {
+        alertify.error(
+            'Sorry! ' + INVENTORY[cart_item].name + ' is out of stock!'
+        );
+    } else {
+        INVENTORY[cart_item].inStock -= 1;
+        CART.push(INVENTORY[cart_item]);
+        console.log(CART);
+        show_inventory();
+
+        alertify.success(
+            'You Added ' + INVENTORY[cart_item].name + ' to your cart!'
+        );
+    }
 }
 function RemoveFromCart(cart_item) {
-    var counter = 0;
-    counter += 1;
-    console.log(counter);
-    console.log(INVENTORY[cart_item].name + ' was removed from the cart.');
+    INVENTORY[cart_item].inStock += 1;
+    show_inventory();
+    alertify.error(
+        'You Removed ' + INVENTORY[cart_item].name + ' from your cart!'
+    );
 }
 // SMALL FUNCTIONS END
 // JQUERY STARTS
@@ -46,6 +63,8 @@ function show_inventory() {
             INVENTORY[i].name +
             '<br>$' +
             INVENTORY[i].price +
+            '<br>In Stock: ' +
+            INVENTORY[i].inStock +
             '<br><button onclick="AddToCart(' +
             i +
             ')">Add To Cart</button><br><button id="RemoveFromCart" onclick="RemoveFromCart(' +
