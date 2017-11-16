c24cec2572a2a6747814e542242efcafc2d90138
Alexandra <afortner@basecampcodingacademy.org>
2017-11-16 13:14:27 -0600

    Made pictures visable by making a picture_url key in the INVENTORY variable.
    I plan to fix the error messages when you try to remove an item from your cart, next.

--- index.js
+++ index.js
@@ -1,19 +1,81 @@
 // MAIN DATA STARTS
 var INVENTORY = [
-    { name: 'Batman: The Killing Joke', price: 16.28, inStock: 20 },
-    { name: 'Batman: Noël', price: 18.39, inStock: 24 },
-    { name: 'Batman: Year One', price: 5.93, inStock: 22 },
-    { name: 'Wolverine Goes To Hell', price: 10.23, inStock: 12 },
-    { name: 'The Death Of Superman', price: 12.19, inStock: 9 },
-    { name: 'Batman: Hush', price: 7.21, inStock: 16 },
-    { name: 'Wonder Woman: The Hiketeia', price: 17.31, inStock: 34 },
-    { name: 'Planet Hulk', price: 23.32, inStock: 6 },
-    { name: 'Avengers: Time Runs Out', price: 21.22, inStock: 10 },
-    { name: 'Deadpool Kills The Marvel Universe', price: 18.43, inStock: 17 },
+    {
+        name: 'Batman: The Killing Joke',
+        price: 16.28,
+        inStock: 20,
+        picture_url:
+            'http://www.thenerdelement.com/wordpress/wp-content/uploads/2016/07/Batman-The-Killing-Joke_Sdtk_Cover_1425px_RGB_300dpi.jpg'
+    },
+    {
+        name: 'Batman: Noël',
+        price: 18.39,
+        inStock: 24,
+        picture_url:
+            'https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Series/12433/12433._SX360_QL80_TTD_.jpg'
+    },
+    {
+        name: 'Batman: Year One',
+        price: 5.93,
+        inStock: 22,
+        picture_url:
+            'https://images-na.ssl-images-amazon.com/images/I/51sroPQ%2B9-L._SY344_BO1,204,203,200_.jpg'
+    },
+    {
+        name: 'Wolverine Goes To Hell',
+        price: 10.23,
+        inStock: 12,
+        picture_url:
+            'https://www298.lunapic.com/do-not-link-here-use-hosting-instead/151085885388182?8615660644'
+    },
+    {
+        name: 'The Death Of Superman',
+        price: 12.19,
+        inStock: 9,
+        picture_url:
+            'https://images-na.ssl-images-amazon.com/images/I/517ostOFBPL._SY344_BO1,204,203,200_.jpg'
+    },
+    {
+        name: 'Batman: Hush',
+        price: 7.21,
+        inStock: 16,
+        picture_url:
+            'https://vignette.wikia.nocookie.net/marvel_dc/images/e/e2/Batman_-_Hush_TPB.jpg/revision/latest?cb=20131103230749'
+    },
+    {
+        name: 'Wonder Woman: The Hiketeia',
+        price: 17.31,
+        inStock: 34,
+        picture_url:
+            'https://vignette.wikia.nocookie.net/marvel_dc/images/7/71/Wonder_Woman_The_Hiketeia_Vol_1_1.jpeg/revision/latest/scale-to-width-down/329?cb=20120722114037'
+    },
+    {
+        name: 'Planet Hulk',
+        price: 23.32,
+        inStock: 6,
+        picture_url:
+            'http://images.comiccollectorlive.com/covers/f00/f005ca70-d137-4838-bc00-5d122d9a4410.jpg'
+    },
+    {
+        name: 'Avengers: Time Runs Out',
+        price: 21.22,
+        inStock: 10,
+        picture_url:
+            'https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/177253/DIG050003_1._SX1280_QL80_TTD_.jpg'
+    },
+    {
+        name: 'Deadpool Kills The Marvel Universe',
+        price: 18.43,
+        inStock: 17,
+        picture_url:
+            'http://vignette3.wikia.nocookie.net/marveldatabase/images/1/19/Deadpool_Kills_the_Marvel_Universe_Vol_1_1.jpg/revision/latest?cb=20120731185747'
+    },
     {
         name: 'Sgt. Fury and his Howling Commandos: Volume 1',
         price: 26.75,
-        inStock: 4
+        inStock: 4,
+        picture_url:
+            'https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/596119/596119._SX1280_QL80_TTD_.jpg'
     }
 ];
 
@@ -29,9 +91,7 @@ function AddToCart(cart_item) {
     } else {
         INVENTORY[cart_item].inStock -= 1;
         CART.push(INVENTORY[cart_item]);
-        console.log(CART);
-        show_inventory();
-
+        draw();
         alertify.success(
             'You Added ' + INVENTORY[cart_item].name + ' to your cart!'
         );
@@ -44,7 +104,7 @@ function RemoveFromCart(cart_item) {
         );
     } else {
         INVENTORY[cart_item].inStock += 1;
-        show_inventory();
+        draw();
         alertify.log(
             'You Removed ' + INVENTORY[cart_item].name + ' from your cart.'
         );
@@ -58,47 +118,47 @@ function RemoveFromCart(cart_item) {
 $('#cart-button').click(function() {
     $('#Inventory-List').html('');
     cart = '';
-    cart = '<button id="Back-Button" onclick="show_inventory()">Back</button>';
+    cart = '<button id="Back-Button" onclick="draw()">Back</button><br>Total:';
     for (n = 0; n < CART.length; n++) {
-        console.log('I got clicked!');
+        var total = CART[n].price;
+        console.log(total);
         cart +=
-            '<p><b>&nbsp;&nbsp;' +
-            CART[n] +
-            '</b><br><button id="RemoveFromCart" onclick="RemoveFromCart(' +
+            '<br><br><b><li>' +
+            CART[n].name +
+            '</li></b><button id="RemoveFromCart" onclick="RemoveFromCart(' +
             n +
-            ')">Remove From Cart</button></p>';
+            ')"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></button>';
     }
     $('#Cart-List').html(cart);
 });
 // .CLICKS END
-// FOR LOOPS START
 
-function show_inventory() {
+// MAIN FUNCTIONS BEGIN
+
+function draw() {
+    $('#Cart-List').html('');
     inventory = '';
     for (i = 0; i < INVENTORY.length; i++) {
         inventory +=
-            '<p><b>&nbsp;&nbsp;' +
+            '<p><div class="row"></b></div><br><div class="col"><button id="AddToCart" onclick="AddToCart(' +
+            i +
+            ')"><i class="fa fa-cart-plus fa-2x" aria-hidden="true"></i></button></div><img src="' +
+            INVENTORY[i].picture_url +
+            '"><div class="col"><b>&nbsp;&nbsp;' +
             INVENTORY[i].name +
             '<br>$' +
             INVENTORY[i].price +
             '<br>In Stock: ' +
             INVENTORY[i].inStock +
-            '<br><button onclick="AddToCart(' +
+            '<br><div class="col"><button id="RemoveFromCart" onclick="RemoveFromCart(' +
             i +
-            ')">Add To Cart</button><br><button id="RemoveFromCart" onclick="RemoveFromCart(' +
-            i +
-            ')">Remove From Cart</button></p>';
+            ')"><i class="fa fa-cart-arrow-down fa-2x" aria-hidden="true"></i></button></div></div></p>';
     }
     $('#Inventory-List').html(inventory);
 }
 
-// FOR LOOPS END
-// MAIN FUNCTIONS BEGIN
-
-function draw() {}
-
 function main() {
-    show_inventory();
+    draw();
 }
 
 // MAIN FUNCTIONS END
